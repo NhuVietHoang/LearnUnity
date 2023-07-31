a005ed426e83499ea6b6b4ee05db73bb2fbf7362
HoangVN <Viethoangnhu@gmail.com>
2023-07-31 09:05:42 +0700

    up project

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3();
+
+    // Bắt đầu được gọi trước khi cập nhật khung hình đầu tiên
+    void Start()
+    { }
+
+    // Cập nhật được gọi một lần trên mỗi khung hình
+    void Update()
+    {
+        if (player != null)
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
